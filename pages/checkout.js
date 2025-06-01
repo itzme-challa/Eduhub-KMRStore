dfa4ce1bdee08953511c344adbb014844cb3fb5b
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-01 14:31:28 +0530

    Create checkout.js

---
+++ pages/checkout.js
@@ -0,0 +1,13 @@
+// pages/checkout.js
+import Checkout from '../components/Checkout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function CheckoutPage() {
+  return (
+    <>
+      <Checkout />
+      <ToastContainer />
+    </>
+  );
+}
