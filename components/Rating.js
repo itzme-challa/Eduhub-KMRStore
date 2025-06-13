95ee5cd48425078657145f828ec83d73eeff398a
itzme-challa <challawaheed4@gmail.com>
2025-06-13 19:53:12 +0530

    Create Rating.js

---
+++ components/Rating.js
@@ -0,0 +1,19 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+
+export default function Rating({ rating, maxRating = 5 }) {
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 >= 0.5;
+  const emptyStars = maxRating - fullStars - (hasHalfStar ? 1 : 0);
+
+  return (
+    <div className="flex items-center">
+      {[...Array(fullStars)].map((_, i) => (
+        <FaStar key={`full-${i}`} className="text-yellow-400" />
+      ))}
+      {hasHalfStar && <FaStarHalfAlt className="text-yellow-400" />}
+      {[...Array(emptyStars)].map((_, i) => (
+        <FaRegStar key={`empty-${i}`} className="text-yellow-400" />
+      ))}
+    </div>
+  );
+}
